e9b743bd52fd340558518aa9241caf0f5a50c345
BusekButBetter <Busek@wlhs.wlwv.k12.or.us>
2022-11-26 14:53:22 -0800

    added constants

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-11-26T14:28:42.721-0800";
+  public static final String APP_BUILD_TIME = "2022-11-26T14:53:10.875-0800";
 }
